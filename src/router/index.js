e462efd92055fa8dc9501638910d152fcc72c03e
Christian Nwamba <nwambachristian@gmail.com>
2017-05-14 20:21:18 +0100

    Outlets & Links

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
+import Home from '@/pages/Home';
+import Cart from '@/pages/Cart';
+import Admin from '@/pages/Admin';
 
 Vue.use(Router);
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin,
+    },
+    {
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart,
     },
   ],
 });
