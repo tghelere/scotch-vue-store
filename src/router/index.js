85c824ce18283218143f48072595fd7ed83547f3
Christian Nwamba <nwambachristian@gmail.com>
2017-06-08 00:05:54 +0100

    Nested and Dynamic Routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/Home';
 import Cart from '@/pages/Cart';
-import Admin from '@/pages/Admin';
+
+import Index from '@/pages/Admin/Index';
+import New from '@/pages/Admin/New';
+import Products from '@/pages/Admin/Products';
+import Edit from '@/pages/Admin/Edit';
 
 Vue.use(Router);
 
@@ -16,7 +20,24 @@ export default new Router({
     {
       path: '/admin',
       name: 'Admin',
-      component: Admin,
+      component: Index,
+      children: [
+        {
+          path: 'new',
+          name: 'New',
+          component: New,
+        },
+        {
+          path: '',
+          name: 'Products',
+          component: Products,
+        },
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit,
+        },
+      ],
     },
     {
       path: '/Cart',
